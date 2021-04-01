ee53e8542bb34d16554342ba53e95e59f923d427
eduardo <eduardo_not@hotmail.com>
2021-04-01 20:51:39 -0300

    refatoração da função de calcular

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,12 @@
-function calcular (n1,n2){
-    console.log(n1+n2);
+const calcular = () => {
+    return{
+        soma: (n1, n2) => {
+            return n1 + n2;
+        },
+        subtrair: (n1, n2) => {
+            return n1 - n2;
+        }
+    }
 }
 
 module.exports = calcular;
