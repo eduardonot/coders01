f29cb45e9a0304b684af15162e8aa87e0f5825b0
eduardo <eduardo_not@hotmail.com>
2021-04-01 20:38:56 -0300

    function calcula 2 valores

---
+++ calculadora.js
@@ -0,0 +1,5 @@
+function calcular (n1,n2){
+    console.log(n1+n2);
+}
+
+module.exports = calcular;
--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 console.log("Caramba");
 console.log(1+1);
+
+const soma = require('./calculadora');
+soma(10, 40);
